a85434aa22c328861c78aca40d2144636e062c14
michaelhtran120 <michaelhtran120@gmail.com>
2021-04-29 14:40:33 -0700

    js update

--- javascript.js
+++ javascript.js
@@ -3,15 +3,30 @@
     function computerPlay(option) {
         return option[Math.floor(Math.random() * option.length)];
     }
-//player input via prompt of rock paper or scissor
-
 
 //function to play RPS with computer.
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return 'You lose! Paper beats Rock';
+    playerSelection=playerSelection.toLowerCase();
+    if ((playerSelection == 'rock' && computerSelection == 'paper') ||
+     (playerSelection == 'paper' && computerSelection == 'scissor') ||
+     (playerSelection == 'scissor' && computerSelection == 'rock')) {
+        return 'You lose!';
+    } else if ((playerSelection == 'rock' && computerSelection == 'rock') ||
+    (playerSelection == 'paper' && computerSelection == 'paper') ||
+    (playerSelection == 'scissor' && computerSelection == 'scissor')) {
+        return 'Tie game';
+    } else {
+        return 'You win!'
     }
 }
+const playerSelection='rock';
+let computerSelection=computerPlay(option);
+
+/*function game() to play 5 round game that keeps score and reports winner 
+   or loser at the end */
+
+
+
 /*  function playRound(playerSelection,computerSelection) {
       if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
            (playerSelection === 'scissor' && computerSelection === 'paper') ||
